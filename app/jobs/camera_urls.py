c4495a6daf219c20c7a9ef39c32f374e5ad54e35
pramod <pramodzayn@gmail.com>
2024-11-08 23:28:55 +0530

    using one cam

--- app/jobs/camera_urls.py
+++ app/jobs/camera_urls.py
@@ -1,12 +1,12 @@
 camera_urls = [ 
-    "rtsp://10.49.25.62:7447/rUxhFZa8mHxzKj8h",
     "rtsp://10.49.25.62:7447/eLIfgPrxgXex3reR",
-    "rtsp://10.49.25.62:7447/cmptwFD7QWOoSCoh",
-    "rtsp://10.49.25.62:7447/mqg7ZavNWwRr49GU",
-    "rtsp://10.49.25.62:7447/CZ660hqXvoi6urj0",
-    "rtsp://10.49.25.62:7447/qAlEjzQdjqfb7wTB",
-    "rtsp://10.49.25.62:7447/tJXgX06qa9iCjvhs",
-    "rtsp://10.49.25.62:7447/F7FuADzLOsnKAMmH",
-    "rtsp://10.49.25.62:7447/3QWKLEruz4heXEm7",
-    "rtsp://10.49.25.62:7447/5ntnVSqNVzxA73om"
+    # "rtsp://10.49.25.62:7447/rUxhFZa8mHxzKj8h",
+    # "rtsp://10.49.25.62:7447/cmptwFD7QWOoSCoh",
+    # "rtsp://10.49.25.62:7447/mqg7ZavNWwRr49GU",
+    # "rtsp://10.49.25.62:7447/CZ660hqXvoi6urj0",
+    # "rtsp://10.49.25.62:7447/qAlEjzQdjqfb7wTB",
+    # "rtsp://10.49.25.62:7447/tJXgX06qa9iCjvhs",
+    # "rtsp://10.49.25.62:7447/F7FuADzLOsnKAMmH",
+    # "rtsp://10.49.25.62:7447/3QWKLEruz4heXEm7",
+    # "rtsp://10.49.25.62:7447/5ntnVSqNVzxA73om"
 ]
